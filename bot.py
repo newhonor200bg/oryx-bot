ea7dc82a08305a025e0140210037bdf28f18f88d
newhonor200bg <newhonor200bg@gmail.com>
2026-05-06 17:52:20 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.getenv("TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Bot is running 😎🔥")
+
+print("Bot running...")
+bot.infinity_polling()
